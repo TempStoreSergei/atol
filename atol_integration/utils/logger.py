df33ea0353e9d4eb7fa946df13eec242a4927cbf
Sergey <sergaiwork@gmail.com>
2025-10-15 13:51:33 +0300

    loger

--- atol_integration/utils/logger.py
+++ atol_integration/utils/logger.py
@@ -7,8 +7,12 @@ from pathlib import Path
 
 def setup_logger(name: str, log_file: str = None, level=logging.INFO):
     """Настроить логгер"""
-    logger = logging.getLogger(name)
-    logger.setLevel(level)
+    logger_instance = logging.getLogger(name)
+    logger_instance.setLevel(level)
+
+    # Предотвращаем дублирование хендлеров, если логгер уже был настроен
+    if logger_instance.hasHandlers():
+        logger_instance.handlers.clear()
 
     formatter = logging.Formatter(
         '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
@@ -17,7 +21,7 @@ def setup_logger(name: str, log_file: str = None, level=logging.INFO):
     # Консольный handler
     console_handler = logging.StreamHandler()
     console_handler.setFormatter(formatter)
-    logger.addHandler(console_handler)
+    logger_instance.addHandler(console_handler)
 
     # Файловый handler
     if log_file:
@@ -25,6 +29,9 @@ def setup_logger(name: str, log_file: str = None, level=logging.INFO):
         log_path.parent.mkdir(parents=True, exist_ok=True)
         file_handler = logging.FileHandler(log_path)
         file_handler.setFormatter(formatter)
-        logger.addHandler(file_handler)
+        logger_instance.addHandler(file_handler)
 
-    return logger
+    return logger_instance
+
+# Создаем и экспортируем логгер по умолчанию для всего приложения
+logger = setup_logger("atol_integration", log_file="atol_integration.log")
